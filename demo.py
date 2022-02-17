ef9507eff589fdc1bcc5628f1aba628f19681277
Lukas Kempf <git@lukas-kempf.de>
2022-02-17 15:56:29 +0100

    Improved demo.py

--- demo.py
+++ demo.py
@@ -10,6 +10,7 @@ _LOGGER.addHandler(logging.StreamHandler())
 async def main():
     logging.getLogger("CasambiBt").setLevel(logging.DEBUG)
 
+    # Discover networks
     print("Searching...")
     addrs = await discover()
     for i, a in enumerate(addrs):
@@ -20,11 +21,19 @@ async def main():
     addr = addrs[selection]
     pwd = input("Enter password: ")
 
+    # Connect to the selected network
     casa = Casambi()
     await casa.connect(addr, pwd)
 
+    # Turn all lights on
+    await casa.turnOn(None)
+    await asyncio.sleep(5)
+
+    # Turn all lights off
     await casa.setLevel(None, 0)
     await asyncio.sleep(1)
+
+    # Print the state of all units
     for u in casa.units:
         print(u.__repr__())
 
